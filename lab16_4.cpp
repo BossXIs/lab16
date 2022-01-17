b79e29c628a7c54b2af807c33244d0d4fd1e091a
BossXI <bossweerawut@hotmail.com>
2022-01-18 00:06:14 +0700

    lab16_4

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void shuffle(int &,int &,int &,int &);
 
+
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
 	
@@ -16,4 +17,24 @@ int main(){
 	}
 	
 	return 0;
+}
+
+void shuffle(int &w,int &x,int &y,int &z){
+
+	int money[] = {50,100,500,1000};
+
+	for(int i = 10; i > 0 ; i--){
+		int j = rand()%4;
+		int k = rand()%4;
+		int t = money[k];
+		money[k]=money[j];
+		money[j]=t;
+
+		if(i==1){
+			w = money[0];
+			x = money[1];
+			y = money[2];
+			z = money[3];
+		}
+	}
 }
